819f73c06398d45f8fe79a4e49861a6ceedbf544
Winnah Gwen Acal <winnahacal@gmail.com>
2017-05-15 22:14:11 +0800

    Add files via upload

--- Lab2/Main.java
+++ Lab2/Main.java
@@ -1,3 +1,4 @@
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -19,12 +20,19 @@ public class Main {
         List<Double> cp = new ArrayList<>();
 
 
+        double subtrahend = 0.5;
+        int des = 2;
+        DecimalFormat numberFormat = new DecimalFormat("#.0");
+        DecimalFormat numberFormat2 = new DecimalFormat("#.00");
         int inSize;
         System.out.println("\t\tMENU\n\t[1] Categorical \n\t[2] Numerical\n\t[3] Quit");
         Scanner scan = new Scanner(System.in);
         Integer choice = scan.nextInt();
 
         if (choice == 2){
+            System.out.println("[1] Integer\t[2] Float");
+            int choice2 = scan.nextInt();
+
             do {
                 try {
                     System.out.println("Input Population Size:");
@@ -37,7 +45,6 @@ public class Main {
                     scan.next();
                 }
             } while (true);
-
 
             System.out.println("Input Sampling Frame (number per Enter):");
             for (int i = 0; i < inSize;) {
@@ -64,64 +71,88 @@ public class Main {
                 }
             }
 
+            int width;
+            double width2;
             double range = max - min;
-            double k = Math.ceil(1 + 3.322*Math.log(inSize));
-            int width = (int)Math.ceil(range/k);
-
-            System.out.println("char:   "+max+" "+ min+ " "+ range+ " " + k +" width: "+width);
-            double limit = min;
-            while (limit < max){
-                limitLeft.add(limit);
-                limit += width;
+            double k = Math.ceil(1 + 3.322*Math.log10(inSize));
+            if (choice2 == 1){
+                width = (int)Math.ceil(range/k);
+                double limit = min;
+                while (limit < max){
+                    limitLeft.add(limit);
+                    limit += width;
+                }
+                System.out.println(width);
+                double limit2 = min + width - 1;
+                while (limit2 <= max){
+                    limitRight.add(limit2);
+                    limit2 += width;
+                }
+            }else {
+                width2 = Math.round((range/k)*10.0)/10.0;
+                double limit = min;
+                while (limit < max){
+                    limitLeft.add(limit);
+                    limit += width2;
+                }
+                System.out.println(width2);
+                double limit2 = min + width2 - 1;
+                while (limit2 <= max){
+                    limitRight.add(limit2);
+                    limit2 += width2;
+                }
             }
-
-            for (int j = 0; j < limitLeft.size(); j++){
-                System.out.print(limitLeft.get(j)+ " ");
-            }
-
-            double limit2 = min + width - 1;
-            while (limit2 <= max){
-                limitRight.add(limit2);
-                limit2 += width;
-            }
+            System.out.println("k: "+ k);
 
             if (limitRight.get(limitRight.size()-1) != max){
-                limitRight.add(max);
-            }
-
-            System.out.println();
-            for (int j = 0; j < limitRight.size(); j++){
-                System.out.print(limitRight.get(j)+ " ");
+                limitRight.add( max);
             }
 
             //true limit
-            for (int j = 0; j < limitLeft.size(); j++){
-                trueLimitLeft.add(limitLeft.get(j) - 0.5);
+            if (des == 1){
+                subtrahend = 0.5;
+            }else{
+                int count1 = 0;
+                float diff = 0;
+                boolean flag = false;
+                for (int j = 0; j < frame.size(); j++){
+                    double value = Math.floor(frame.get(j));
+                    diff = (float)(frame.get(j) - value);
+                    String counts = String.valueOf(diff);
+                    if (diff > 0.0){
+                        if (counts.length() - 2 > count1){
+                            count1 = counts.length() - 2;
+                            System.out.println(count1);
+                        }
+                    }
+                }
+
+                System.out.println(count1);
+
+                for (int j = 0; j < count1; j++){
+                    subtrahend /= 10;
+                    System.out.println(subtrahend);
+                }
             }
 
-            System.out.println();
-            for (int j = 0; j < trueLimitLeft.size(); j++){
-                System.out.print(trueLimitLeft.get(j)+ " ");
+
+            //TODO this
+            System.out.println("sub: "+subtrahend);
+            for (int j = 0; j < limitLeft.size(); j++){
+                trueLimitLeft.add(limitLeft.get(j) - subtrahend);
             }
 
             for (int j = 0; j < limitRight.size(); j++){
-                trueLimitRight.add(limitRight.get(j) + 0.5);
+                trueLimitRight.add(limitRight.get(j) + subtrahend);
             }
 
-            System.out.println();
-            for (int j = 0; j < trueLimitRight.size(); j++){
-                System.out.print(trueLimitRight.get(j)+ " ");
-            }
+
 
             //midpoint
             for (int j = 0; j < limitLeft.size(); j++){
                 midpoint.add((limitLeft.get(j) + limitRight.get(j))/2);
             }
 
-            System.out.println();
-            for (int j = 0; j < midpoint.size(); j++){
-                System.out.print(midpoint.get(j)+ " ");
-            }
 
             //frequency
             for (int j = 0; j < limitLeft.size(); j++){
@@ -134,20 +165,11 @@ public class Main {
                 frequency.add(count);
             }
 
-            System.out.println();
-            for (int j = 0; j < frequency.size(); j++){
-                System.out.print(frequency.get(j)+ " ");
-            }
 
             //percent
             for (int j = 0; j < frequency.size(); j++){
-                double pp = (double)((frequency.get(j)*100)/inSize);
+                double pp = ((double)frequency.get(j)/inSize)*100;
                 percent.add(pp);
-            }
-
-            System.out.println();
-            for (int j = 0; j < percent.size(); j++){
-                System.out.print(percent.get(j)+ " ");
             }
 
 
@@ -157,10 +179,6 @@ public class Main {
                 cf.add(cff);
             }
 
-            System.out.println();
-            for (int j = 0; j < cf.size(); j++){
-                System.out.print(cf.get(j)+ " ");
-            }
 
             double cpp = 0;
             for (int j = 0; j < percent.size(); j++){
@@ -168,19 +186,15 @@ public class Main {
                 cp.add(cpp);
             }
 
-            System.out.println();
-
-            for (int j = 0; j < cp.size(); j++){
-                System.out.print(cp.get(j)+ " ");
-            }
-
 
             for (int j = 0; j < limitLeft.size(); j++){
                 if (j == 0){
                     System.out.println("\nClass Limit\tTrue Limit\tMidp\tFre\tPerce\tCFre\tCPer");
                 }
-                System.out.print(limitLeft.get(j)+"-"+limitRight.get(j)+"\t"+trueLimitLeft.get(j)+"-"+trueLimitRight.get(j)
-                        + "\t"+midpoint.get(j)+"\t"+frequency.get(j)+"\t"+percent.get(j)+"\t"+cf.get(j)+"\t"+cp.get(j)+"\n");
+                System.out.print(numberFormat.format(limitLeft.get(j))+"-"+numberFormat.format(limitRight.get(j))+"\t"
+                        +numberFormat2.format(trueLimitLeft.get(j))+"-"+numberFormat2.format(trueLimitRight.get(j))
+                        + "\t"+numberFormat.format(midpoint.get(j))+"\t"+frequency.get(j)+"\t"+ numberFormat.format(percent.get(j))
+                        +"\t"+cf.get(j)+"\t"+ numberFormat.format(cp.get(j))+"\n");
             }
 
         }else if (choice ==1){
